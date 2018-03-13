797401c1e0500cd442e4f581e1cdf34ea8b02e1d
Rohan Pawar <daupawar@gmail.com>
2018-03-13 18:56:51 +0530

    Initial commit

---
+++ app/src/main/java/ron/bookmarseekbar/MainActivity.java
@@ -0,0 +1,160 @@
+package ron.bookmarseekbar;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
+
+import ron.seekbarmarker.SeekbarMarker;
+
+public class MainActivity extends AppCompatActivity
+        implements
+        SeekBar.OnSeekBarChangeListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnSeekCompleteListener, SurfaceHolder.Callback {
+
+    private SurfaceHolder holder;
+    private MediaPlayer mp;
+    private SeekbarMarker seekbar;
+    private Handler handler = new Handler();
+    private Utilities utils = new Utilities();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekbar = findViewById(R.id.timeline);
+        seekbar.setOnSeekBarChangeListener(this);
+
+        SurfaceView mPreview = findViewById(R.id.surface);
+        holder = mPreview.getHolder();
+        holder.addCallback(this);
+
+        // add bookmarks
+        seekbar.addBookmark(50 * 1000, R.drawable.orange_flag);
+        seekbar.addBookmark(120 * 1000,R.drawable.black_flag);
+        seekbar.addBookmark(170 * 1000,R.drawable.white_flag);
+
+        // event handlers
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekbar.addBookmark(mp.getCurrentPosition());
+            }
+        });
+
+        ((ToggleButton) findViewById(R.id.toggleButton1)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mp.start();
+                } else {
+                    mp.pause();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        synchronized (mp) {
+            handler.removeCallbacks(mUpdateTimeTask);
+            mp.release();
+        }
+    }
+
+    private void setMeidaPlayer() {
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.leesa);
+        mp = new MediaPlayer();
+        try {
+            mp.setDataSource(getApplicationContext(), uri);
+            mp.setDisplay(holder);
+            mp.setOnPreparedListener(this);
+            mp.setOnSeekCompleteListener(this);
+            mp.prepare();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateProgressBar() {
+        handler.postDelayed(mUpdateTimeTask, 100);
+    }
+
+    /**
+     * Background Runnable thread
+     */
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            try {
+                synchronized (mp) {
+                    long totalDuration = mp.getDuration();
+                    long currentDuration = mp.getCurrentPosition();
+
+                    // Updating progress bar
+                    int progress = utils.getProgressPercentage(currentDuration, totalDuration);
+                    seekbar.setProgress(progress);
+
+                    // Running this thread after 100 milliseconds
+                    handler.postDelayed(this, 100);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        setMeidaPlayer();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (fromUser) {
+            mp.seekTo(utils.progressToTimer(progress, mp.getDuration()));
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+        updateProgressBar();
+        seekbar.setVideoDuration(mp.getDuration());
+    }
+
+    @Override
+    public void onSeekComplete(MediaPlayer mp) {
+    }
+}
---
+++ seekbarmarker/src/main/java/ron/seekbarmarker/SeekbarMarker.java
@@ -0,0 +1,124 @@
+package ron.seekbarmarker;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.DrawableRes;
+import android.util.AttributeSet;
+
+import java.util.ArrayList;
+
+/**
+ * ༼-つ-◕_◕-༽つ
+ *
+ * @author Rohan on 13/03/18.
+ */
+
+public class SeekbarMarker extends android.support.v7.widget.AppCompatSeekBar {
+
+    private ArrayList<Bookmark> bookmarks = new ArrayList<>();
+    private ArrayList<Rect> bookmarksRect = new ArrayList<>();
+
+    private float positionPerSecond = 0;
+    private Paint bookmarkPaint;
+    private int videoDuration;
+
+    private int bookmarkColor;
+    private int bookmarkWidth;
+
+    public SeekbarMarker(Context context) {
+        super(context);
+        init();
+    }
+
+    public SeekbarMarker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.SeekbarMarker,
+                0, 0);
+
+        bookmarkColor = a.getColor(R.styleable.SeekbarMarker_marker_color, 0x00);
+        bookmarkWidth = a.getInt(R.styleable.SeekbarMarker_marker_width, 2);
+
+        init();
+    }
+
+    private void init() {
+        bookmarkPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        bookmarkPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        bookmarkPaint.setColor(bookmarkColor);
+        bookmarkPaint.setStrokeWidth(bookmarkWidth);
+    }
+
+    public void addBookmark(int millisec) {
+        bookmarks.add(new Bookmark(millisec));
+        bookmarksRect.add(new Rect(0, 0, 0, 0));
+        invalidate();
+    }
+
+    public void addBookmark(int millisec, int drawable) {
+        bookmarks.add(new Bookmark(millisec, drawable));
+        bookmarksRect.add(new Rect(0, 0, 0, 0));
+        invalidate();
+    }
+
+
+    public void setVideoDuration(int duration) {
+        this.videoDuration = duration;
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (positionPerSecond == 0 && videoDuration != 0) {
+            positionPerSecond = (float) getWidth() / (float) videoDuration;
+        }
+
+
+        for (int i = 0; i < bookmarks.size(); i++) {
+            Bookmark bookmark = bookmarks.get(i);
+            Rect rect = bookmarksRect.get(i);
+            Bitmap bitmap = bookmark.getMarkerBitmap();
+
+            if (rect.left == 0 && rect.right == 0 && positionPerSecond != 0) {
+                rect.bottom = getHeight();
+                rect.left = (int) (positionPerSecond * bookmark.getTime_milsec());
+                rect.right = rect.left + 30;
+            }
+
+            if (bitmap == null) {
+                canvas.drawRect(rect, bookmarkPaint);
+            } else {
+                canvas.drawBitmap(bookmark.getMarkerBitmap(), null, rect, null);
+            }
+        }
+    }
+
+    private class Bookmark {
+        private int time_milsec;
+        private int drawableRes;
+
+        public Bookmark(int time_milsec) {
+            this.time_milsec = time_milsec;
+        }
+
+        public Bookmark(int time_milsec, @DrawableRes int markerImage) {
+            this.time_milsec = time_milsec;
+            drawableRes = markerImage;
+        }
+
+        public int getTime_milsec() {
+            return time_milsec;
+        }
+
+        public Bitmap getMarkerBitmap() {
+            return BitmapFactory.decodeResource(getContext().getResources(), drawableRes);
+        }
+    }
+}
